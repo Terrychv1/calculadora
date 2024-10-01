c6b41221dfe94fe871a22abc3c7197bda54c174b
Terrychv1 <zx23student3263@colegiovalledelmiro.es>
2024-10-01 13:18:25 +0200

    Funcionalidad de sumar

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+# funcion sumar
+
+def sumar ( a, b):
+    return a + b
+
+a = float(input("Introduce el primer número: "))
+b = float(input("Introduce el segundo número: "))
+
+print("Calculadora")
+print("1. Sumar")
+
+while True:
+        opcion = input("Selecciona una opción (1/2/3) o 'salir' para terminar: ")
+
+
+
+        if opcion == '1':
+            print("Resultado:", sumar(a, b))
