b71a0481e1e209d5019601ed3730ef5567ee0bb2
Terrychv1 <zx23student3263@colegiovalledelmiro.es>
2024-10-01 13:24:11 +0200

    Funcionalidad de multiplicar

--- calculadora.py
+++ calculadora.py
@@ -2,19 +2,31 @@
 
 def sumar ( a, b):
     return a + b
+
 def restar ( a, b):
     return a - b
+
+def multiplicar ( a, b):
+    return a * b
 
 a = float(input("Introduce el primer número: "))
 b = float(input("Introduce el segundo número: "))
 
 print("Calculadora")
 print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
 
 while True:
         opcion = input("Selecciona una opción (1/2/3) o 'salir' para terminar: ")
+
+
 
         if opcion == '1':
             print("Resultado:", sumar(a, b))
         elif opcion == '2':
             print("Resultado:", restar(a, b))
+        elif opcion == '3':
+            print("Resultado:", multiplicar(a, b))
+        else:
+            print("Opción no válida. Intenta de nuevo.")
