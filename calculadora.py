3456bbdc578bf07fbcf92259f663682233193927
Terrychv1 <zx23student3263@colegiovalledelmiro.es>
2024-10-01 13:21:13 +0200

    Funcionalidad de restar

--- calculadora.py
+++ calculadora.py
@@ -2,6 +2,8 @@
 
 def sumar ( a, b):
     return a + b
+def restar ( a, b):
+    return a - b
 
 a = float(input("Introduce el primer número: "))
 b = float(input("Introduce el segundo número: "))
@@ -12,7 +14,7 @@ print("1. Sumar")
 while True:
         opcion = input("Selecciona una opción (1/2/3) o 'salir' para terminar: ")
 
-
-
         if opcion == '1':
             print("Resultado:", sumar(a, b))
+        elif opcion == '2':
+            print("Resultado:", restar(a, b))
